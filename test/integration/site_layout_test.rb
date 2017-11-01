d8b04486eaae928da74531d664007b4b95c2e67d
Malcolm Groves <macgroves@gmail.com>
2017-11-01 18:44:35 -0400

    Implement update and destroy with admin privileges

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,10 +13,26 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+
+    # don't include these links if not logged in
+    assert_select "a[href=?]", users_path, count: 0
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", edit_user_path(@user), count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", login_path
+
+
+    log_in_as(@user)
+    get user_path(@user)
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", edit_user_path
+    assert_select "a[href=?]", user_path
   end
 
 end
