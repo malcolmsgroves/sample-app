4a566d012febe089810e65c44bbf1d230d2e4ff0
Malcolm Groves <macgroves@gmail.com>
2017-11-08 14:33:50 -0500

    Implemented the status feed and following users

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -13,6 +13,12 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
+
+    assert_select 'a[href=?]', following_user_path
+    assert_select 'a[href=?]', followers_user_path
+    assert_select 'strong#followers', text: "#{@user.followers.count}"
+    assert_select 'strong#following', text: "#{@user.following.count}"
+
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination'
     @user.microposts.paginate(page: 1).each do |micropost|
